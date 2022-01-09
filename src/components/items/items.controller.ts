1a9680b15d5397771480e8c7e29315659cb2a9bb
Jo-Sundaram <jsund301@gmail.com>
2022-01-08 21:20:23 -0500

    feat: add routes to add, remove and sell items

--- src/components/items/items.controller.ts
+++ src/components/items/items.controller.ts
@@ -1,18 +1,17 @@
 import { Bind, Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Res, ValidationPipe } from '@nestjs/common';
-import { ItemDTO } from './dto/item.dto';
+import { ItemDTO, UpdateItemDTO, ItemQuantityDTO } from './dto/item.dto';
 import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
     constructor(private itemService: ItemsService){}
 
-    @Post('add')
+    @Post('create')
     @Bind(Res(), Body(new ValidationPipe))
     async addItem(res, body: ItemDTO) {
-      const item = await this.itemService.create(body);
+      const item = await this.itemService.createItem(body);
       res.status(HttpStatus.CREATED).send(item);
     }
-
 
     @Get()
     @Bind(Res())
@@ -30,15 +29,36 @@ export class ItemsController {
 
     @Patch(':id')
     @Bind(Res(), Param(), Body(new ValidationPipe))
-    async udpate(res, params, body: ItemDTO) {
-      const item = await this.itemService.update(params.id, body);
+    async updateItem(res, params, body: UpdateItemDTO) {
+      const item = await this.itemService.updateItem(params.id, body);
+      res.status(HttpStatus.OK).send(item);
+    }
+
+    @Patch(':id/add')
+    @Bind(Res(), Param(), Body(new ValidationPipe))
+    async addToItem(res, params, body: ItemQuantityDTO) {
+      const item = await this.itemService.add(params.id, body.quantity);
+      res.status(HttpStatus.OK).send(item);
+    }
+
+    @Patch(':id/remove')
+    @Bind(Res(), Param(), Body(new ValidationPipe))
+    async removeFromItem(res, params, body: ItemQuantityDTO) {
+      const item = await this.itemService.remove(params.id, body.quantity);
+      res.status(HttpStatus.OK).send(item);
+    }
+
+    @Patch(':id/sell')
+    @Bind(Res(), Param(), Body(new ValidationPipe))
+    async sellFromItem(res, params, body: ItemQuantityDTO) {
+      const item = await this.itemService.remove(params.id, body.quantity);
       res.status(HttpStatus.OK).send(item);
     }
 
     @Delete(':id')
     @Bind(Res(), Param())
     async delete(res, params){
-        const item = await this.itemService.delete(params.id);
+        const item = await this.itemService.deleteItem(params.id);
         res.status(HttpStatus.NO_CONTENT).send(item);
     }
 
--- src/components/items/items.service.ts
+++ src/components/items/items.service.ts
@@ -1,35 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Item, ItemDocument } from './items.schema';
+import { Item, ItemDocument, ItemSchema } from './items.schema';
 
 @Injectable()
 export class ItemsService {
     constructor(@InjectModel(Item.name) private itemModel: Model<ItemDocument>) {}
 
-    async create(item): Promise<Item> {
+    async createItem(item): Promise<ItemDocument> {
+        item.price = Number(item.price.toFixed(2))
         const createdItem = new this.itemModel(item)
         return await createdItem.save();
     }
 
-    async findAll(): Promise<Item[]> {
+    async findAll(): Promise<ItemDocument[]> {
         return await this.itemModel.find().exec();
     }
 
-    async findOne(id): Promise<Item> {
-        return await this.itemModel.findOne({_id: id}).exec();
+    async findOne(id): Promise<ItemDocument> {
+        const item = await this.itemModel.findOne({_id: id}).exec();
+        return item;
     }
 
-    async update(id, updateBody): Promise<Item> {
+    async updateItem(id, updateBody): Promise<ItemDocument> {
         const item = await this.itemModel.findById(id);
         Object.assign(item, updateBody)
         return await item.save();
     }
 
-    async delete(id): Promise<Item>{
+    async add(id, quantity): Promise<ItemDocument> {
+        const item = await this.itemModel.findOne({_id: id}).exec();
+        item.quantity += quantity;
+        return await item.save();
+    }
+
+    async remove(id, quantity): Promise<ItemDocument> {
+        const item = await this.itemModel.findById(id);
+        if (item.quantity < quantity){
+            throw new HttpException(`Cannot remove more than existing quantity: ${item.quantity}`, HttpStatus.BAD_REQUEST);
+        }
+        item.quantity -= quantity;
+        return await item.save();
+    }
+
+
+    async sell(id, quantity): Promise<ItemDocument> {
+        const item = await this.itemModel.findById(id);
+        if (item.quantity < quantity){
+            throw new HttpException(`Cannot sell more than existing quantity: ${item.quantity}`, HttpStatus.BAD_REQUEST);
+        }
+        item.quantity -= quantity;
+        item.sold += quantity;
+        return await item.save();
+    }
+
+
+    async deleteItem(id): Promise<ItemDocument>{
         const item = this.itemModel.findById(id).exec();
         await this.itemModel.deleteOne({_id: id}).exec();
         return item;
     }
-
 }

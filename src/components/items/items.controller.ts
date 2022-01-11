1c52f0ace2a783ec0d3f8058d01bc1d737d509a0
Jo-Sundaram <jsund301@gmail.com>
2022-01-10 22:38:47 -0500

    feat: add  api and response descriptors

--- src/components/items/items.controller.ts
+++ src/components/items/items.controller.ts
@@ -1,6 +1,11 @@
 import { Bind, Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Query, Req, Res, ValidationPipe } from '@nestjs/common';
 import { ApiBody, ApiCreatedResponse, ApiNoContentResponse, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
-import { ItemDTO, UpdateItemDTO, ItemQuantityDTO, ItemIdDTO, ActionEnum, ItemActionDTO } from './dto/item.dto';
+import { Response } from 'express';
+import { ItemDTO } from './dto/create-item.dto';
+import { ItemActionDTO } from './dto/item-action.dto';
+import { ItemIdDTO } from './dto/item-id.dto';
+import { ItemQuantityDTO } from './dto/item-quantity.dto';
+import { UpdateItemDTO } from './dto/update-item.dto';
 import { ItemsService } from './items.service';
 
 @Controller('items')
@@ -8,39 +13,40 @@ import { ItemsService } from './items.service';
 export class ItemsController {
     constructor(private readonly itemService: ItemsService){}
 
-    @Post('create')
-    @ApiBody({
-      description: 'New item record',
-      type: ItemDTO
-    })
+    /**
+     * Create an item
+     */
     @ApiCreatedResponse({
-      description: 'The item has been successfully created.',
+      description: 'Successfully creates item',
       type: ItemDTO,
     })
+    @Post('create')
     @Bind(Res(), Body(new ValidationPipe))
-    async addItem(res, body: ItemDTO) {
+    async addItem(res: Response, body: ItemDTO) {
       const item = await this.itemService.createItem(body);
       res.status(HttpStatus.CREATED).send(item);
     }
 
-    @Get()
-    @ApiOkResponse({
-      description: 'All item records retrieved.',
+    /**
+     * Query for items
+     */
+     @ApiOkResponse({
+      description: 'Successfully retrieves queried items',
       type: [ItemDTO],
     })
+    @Get()
     @Bind(Res(), Query())
     async findAll(res, query) {
       const items = await this.itemService.queryItems(query);
-    res.status(HttpStatus.OK).send(items);
+      res.status(HttpStatus.OK).send(items);
     }
 
+    /**
+     * Query for items using query string values
+     */
     @Get(':id')
-    @ApiParam({
-      name: 'id',
-      description: 'The id of the item',
-    })
     @ApiOkResponse({
-      description: 'Successfully retrieved item record.',
+      description: 'Successfully retrieves item',
       type: ItemDTO,
     })
     @Bind(Res(), Param(new ValidationPipe))
@@ -51,7 +57,7 @@ export class ItemsController {
 
     @Patch(':id')
     @ApiOkResponse({
-      description: 'Successfully updated item record.',
+      description: 'Successfully updates item',
       type: ItemDTO,
     })
     @Bind(Res(), Param(new ValidationPipe), Body(new ValidationPipe))
@@ -61,34 +67,20 @@ export class ItemsController {
     }
 
     @Patch(':id/:action')
-    @ApiParam({
-      name: 'id',
-      description: 'The id of the item',
-    })
-    @ApiParam({
-      name: 'action',
-      description: 'Action to perform on the item',
-      enum: ActionEnum,
-      enumName: 'Action'
-    })
     @ApiOkResponse({
-      description: 'Successfully updated item record.',
+      description: 'Successfully updates item',
       type: ItemDTO,
     })
     @Bind(Res(), Param(new ValidationPipe), Body(new ValidationPipe))
-    async addToItem(res, params: ItemActionDTO, body: ItemQuantityDTO) {
+    async action(res, params: ItemActionDTO, body: ItemQuantityDTO) {
       const item = await this.itemService.action(params.id, params.action, body.quantity);
       res.status(HttpStatus.OK).send(item);
     }
 
 
     @Delete(':id')
-    @ApiParam({
-      name: 'id',
-      description: 'The id of the item',
-    })
     @ApiNoContentResponse({
-      description: 'Successfully deleted item record.',
+      description: 'Successfully deletes item',
     })
     @Bind(Res(),Param(new ValidationPipe))
     async delete(res, params: ItemIdDTO){

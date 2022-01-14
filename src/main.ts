9df0b3c69b69200a458dddef4e8a8a58b5a4ac95
Jo-Sundaram <jsund301@gmail.com>
2022-01-13 22:35:30 -0500

    chore: use port env variable

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, {include: [ItemsModule]});
 
   SwaggerModule.setup('api/docs/inventory-tracker', app, document);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port);
 }
 bootstrap();
